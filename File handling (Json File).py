27eaa34e924e6c6b60ee6b8817efadc1a1fc38a1
Ali_206 <alibepari206@gmail.com>
2026-04-17 15:10:34 +0530

    Add files via upload

---
+++ File handling (Json File).py
@@ -0,0 +1,9 @@
+
+
+[
+    {"name":"Ali","mark":"98","age":19},
+     {"name":"Toast","mark":"90","age":19},
+      {"name":"Sergio","mark":"80","age":18},
+       {"name":"Ben","mark":"79","age":19}
+    
+]
